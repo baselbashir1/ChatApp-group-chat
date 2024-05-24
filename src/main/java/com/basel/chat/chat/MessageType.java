e92fba6fa42fb9ea96a44e19c7828f3f3d4a8aa3
Mhd Basel Bashir <u13472@isoint.net>
2024-05-25 00:49:17 +0300

    first commit

---
+++ src/main/java/com/basel/chat/chat/MessageType.java
@@ -0,0 +1,9 @@
+package com.basel.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+
+}
---
+++ src/main/java/com/basel/chat/config/WebSocketEventListener.java
@@ -0,0 +1,34 @@
+package com.basel.chat.config;
+
+import com.basel.chat.chat.ChatMessage;
+import com.basel.chat.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            ChatMessage chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+
+}
